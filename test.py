a131a5d6930090f5e972046e2de4734b276c082a
Marcin Juszkiewicz <marcin@juszkiewicz.com.pl>
2021-05-13 18:16:22 +0200

    add simple script showing usage

---
+++ test.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+import syscalls
+
+system_calls = syscalls.syscalls_dict()
+
+for test_call in ['openat', 'osf_uadmin', 'nosuchcall']:
+    try:
+        print(system_calls[test_call])
+    except syscalls.NoSuchSystemCall:
+        print(f"No such system call '{test_call}' on any architecture")
+    except syscalls.NotSupportedSystemCall:
+        print(f"System call '{test_call}' is not supported on this "
+              "architecture")
