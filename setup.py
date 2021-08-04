9c7808f1c3c32e9a4c1b9e8c5a5bf3abe8ca5068
Marcin Juszkiewicz <marcin@juszkiewicz.com.pl>
2021-08-04 18:32:54 +0200

    5.13.2

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="system-calls",
-    version="5.13.1",
+    version="5.13.2",
     author="Marcin Juszkiewicz",
     author_email="marcin-python@juszkiewicz.com.pl",
     description="Python module to check for system call numbers",
