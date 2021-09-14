b751a8d72dd53e6bb1a39fa4cb01f9975391884e
Marcin Juszkiewicz <marcin@juszkiewicz.com.pl>
2021-09-14 14:04:01 +0200

    added script to check system calls

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="system-calls",
-    version="5.15.0",
+    version="5.15.1",
     author="Marcin Juszkiewicz",
     author_email="marcin-python@juszkiewicz.com.pl",
     description="Python module to check for system call numbers",
@@ -25,4 +25,5 @@ setuptools.setup(
     packages=["syscalls", "syscalls/tables/"],
     python_requires=">=3.6",
     license_files=["LICENSE"],
+    scripts=["bin/syscall"],
 )
